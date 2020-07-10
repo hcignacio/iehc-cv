452b1538e3869c43770d28fa18269a0286bb68b3
hcignacio <46901747+hcignacio@users.noreply.github.com>
2020-07-10 18:05:20 -0300

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./Screens/Home/Home";
+import Projects from './Screens/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <h1>HOLA</h1>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+        </Switch>
+      </Router>
     </div>
   );
 }
